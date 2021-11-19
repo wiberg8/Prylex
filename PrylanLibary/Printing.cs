90cee146f64a8274f0e3d6350fcf9fcd9a2fd75a
wiberg8 <jesper.dahlberg.wiberg@gmail.com>
2021-11-19 10:25:13 +0100

    printing

--- PrylanLibary/Printing.cs
+++ PrylanLibary/Printing.cs
@@ -1,10 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Collections.Specialized;
+using System.Drawing;
+using System.Drawing.Printing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using DYMO.Label.Framework;
+using BarcodeLib.Barcode;
 using PrylanLibary.Models;
 
 namespace PrylanLibary
@@ -12,47 +14,81 @@ namespace PrylanLibary
     public static class Printing
     {
         public static string Exception { get; set; }
-        private const string labelKombo = "Prylan.label";
-        private const string labelSerieNr = "SerieNr.label";
 
-        public static bool PrintSerieNrLabel(string serieNr, string printer)
+        public static void PrintSerieNrLabel(string serieNr, string printer)
         {
-            try
+            PrintDocument printDocument = new PrintDocument();
+            printDocument.PrintPage += (object sender, PrintPageEventArgs e) =>
             {
-                ILabel label = Label.Open(labelSerieNr);
-                label.SetObjectText("dynSerieNr", serieNr);
-                label.Print(printer);
-            }
-            catch (Exception ex)
-            {
-                Exception = ex.Message;
-                return false;
-            }
-            return true;
+                Graphics g = e.Graphics;
+                Font font = new Font("Arial", 10);
+                SolidBrush brush = new SolidBrush(System.Drawing.Color.Black);
+
+                try
+                {
+                    PDF417 PDF417 = new PDF417
+                    {
+                        Data = serieNr,
+                        UOM = UnitOfMeasure.CM,
+                        BackgroundColor = Color.White,
+                        ModuleColor = Color.Black,
+                        ImageFormat = System.Drawing.Imaging.ImageFormat.Png,
+                        ImageWidth = 3
+                    };
+                    PDF417.drawBarcode("barcode.jpg");
+                    g.DrawImage(Image.FromFile("barcode.jpg", true), new Point(3, 39));
+                }
+                catch (Exception ex)
+                {
+                    Logger.Log(ex);
+                }
+            };
+            printDocument.DefaultPageSettings.Landscape = true;
+            printDocument.PrinterSettings.PrinterName = printer;
+            printDocument.Print();
         }
 
-        public static bool PrintLabel(Artikel artikel, Person person, string printer)
+
+        public static void PrintLabel(Artikel artikel, Person person, string printer)
         {
-            try
+            PrintDocument printDocument = new PrintDocument();
+            printDocument.PrintPage += (object sender, PrintPageEventArgs e) =>
             {
-                ILabel label = Label.Open(labelKombo);
-                label.SetObjectText("dynDatorNamn", artikel.DatorNamn);
-                label.SetObjectText("dynSerieNr", artikel.SerieNr);
-                label.SetObjectText("dynNamn", person.FullName);
-                label.SetObjectText("dynTillhorighet", person.Tillhorighet);
-                label.Print(printer);
-            }
-            catch (Exception ex)
-            {
-                Exception = ex.Message;
-                return false;
-            }
-            return true;
+                Graphics g = e.Graphics;
+                Font font = new Font("Arial", 10, FontStyle.Bold);
+                SolidBrush brush = new SolidBrush(System.Drawing.Color.Black);
+
+                g.DrawString("Stenhamraskolan", font, brush, new Rectangle(3, 5, 200, 100));
+                g.DrawString($"{artikel.DatorNamn} / {person.Tillhorighet}", font, brush, new Rectangle(3, 25, 200, 100));
+                g.DrawString($"{person.Fornamn} {person.Efternamn}", font, brush, new Rectangle(3, 43, 300, 100));
+
+                try
+                {
+                    PDF417 PDF417 = new PDF417
+                    {
+                        Data = artikel.SerieNr,
+                        UOM = UnitOfMeasure.CM,
+                        BackgroundColor = Color.White,
+                        ModuleColor = Color.Black,
+                        ImageFormat = System.Drawing.Imaging.ImageFormat.Png,
+                        ImageWidth = 3
+                    };
+                    PDF417.drawBarcode("barcode.jpg");
+                    g.DrawImage(Image.FromFile("barcode.jpg", true), new Point(3, 63));
+                }
+                catch(Exception ex)
+                {
+                    Logger.Log(ex);
+                }
+            };
+            printDocument.DefaultPageSettings.Landscape = true;
+            printDocument.PrinterSettings.PrinterName = printer;
+            printDocument.Print();
         }
 
         public static IEnumerable<string> GetPrinters()
         {
-            return System.Drawing.Printing.PrinterSettings.InstalledPrinters.Cast<string>();
+            return PrinterSettings.InstalledPrinters.Cast<string>();
         }
     }
 }
